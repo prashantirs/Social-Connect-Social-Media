7d96d014c6926a8552d7856becea375d6a03035e
Prashant Srivastava <prashantsrivastava381@gmail.com>
2022-03-06 02:57:45 +0530

    Social Media APP Created

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const app=require('./app');
+const { connectDatabse } = require('./config/database');
+const  cloudinary = require('cloudinary');
+connectDatabse();
+
+cloudinary.config({
+  cloud_name:process.env.CLOUDINARY_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET,
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+  })
